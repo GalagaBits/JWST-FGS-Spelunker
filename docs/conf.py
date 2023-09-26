72d4099cd0e884eb0190076bfa8a1fe90fe057e6
Derod Deal <dealderod@gmail.com>
2023-09-26 11:55:03 -0400

    Readthedocs fix API 5

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = "spklogo2.png"
+html_logo = "spklogo3.png"
 
 html_theme = 'agogo'
 
