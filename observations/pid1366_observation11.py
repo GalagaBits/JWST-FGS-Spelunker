27e680a50b3d36e5165708ee7dfdcb50fa62d3ca
GalagaBits <dealderod@gmail.com>
2024-03-18 02:06:43 -0400

    cleaned repo

--- observations/pid1366_observation11.py
+++
@@ -1,9 +0,0 @@
-from astropy.io import fits
-import fgs_spelunker_ver_0405
-
-spk = fgs_spelunker_ver_0405.fgs_splelunker('/Users/ddeal/JWST-Treasure-Chest-2023/', 1366)
-
-spk.download('1366', obs_num=11,)
-
-table_gauss2d_fit = spk.gauss2d_fit(pid1366_array, ncpus=6) # ncpus sets the number of cpu cores your computer has. Defaults to 4 cores.
-table_gauss2d_fit.write('/Users/ddeal/JWST-Treasure-Chest-2023/pid1366_observation11_01.dat', format='ascii', overwrite=True)
--- observations/pid1717_observation01.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('/Users/ddeal/JWST-FGS-Spelunker/JWST-FGS-Spelunker-main/')
-import spelunker.spelunker as spelunker
-from astropy.io import fits
-
-spk = spelunker.load('/Users/ddeal/JWST-Treasure-Chest/',pid=1717, obs_num=1)
-table_gauss2d_fit = spk.gauss2d_fit(spk.fg_array, ncpus=6) # ncpus sets the number of cpu cores your computer has. Defaults to 4 cores.
-table_gauss2d_fit.write('/Users/ddeal/JWST-Treasure-Chest/pid1717_observation01.dat', format='ascii', overwrite=True)
