6f0963fb48f2c09d4f3d1b4a6c7619e135cda53a
GalagaBits <dealderod@gmail.com>
2024-03-19 13:11:49 -0400

    documentation changes

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'Derod Deal & Nestor Espinoza'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
